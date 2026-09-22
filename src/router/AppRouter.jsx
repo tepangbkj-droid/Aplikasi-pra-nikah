5fdbf3cecb478366137ce7fc24d232f67649e5e2
tepangbkj-droid <tepangbkj@gmail.com>
2026-09-22 08:10:31 +0700

    Add files via upload

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,1 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Dashboard from "../pages/Dashboard";
+import TodoPage from "../pages/TodoPage";
+import GoldPage from "../pages/GoldPage";
+import VendorPage from "../pages/VendorPage";
+import ItemsPage from "../pages/ItemsPage";
+import FinancePage from "../pages/FinancePage";
 
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/checklist" element={<TodoPage />} />
+          <Route path="/emas" element={<GoldPage />} />
+          <Route path="/vendor" element={<VendorPage />} />
+          <Route path="/barang" element={<ItemsPage />} />
+          <Route path="/keuangan" element={<FinancePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
